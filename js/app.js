41ddbca340d915cee42ac97298e59773c081c8dc
Alberto Francesco Motta <alberto.motta.86@gmail.com>
2015-12-28 22:15:04 +0100

    feat: filterPlaces

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-// GoogleMap init
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 45.466342, lng: 9.188288},
-    zoom: 13,
-    disableDefaultUI: true
-  });
-}
-
 // knockout
 
 // models
@@ -16,18 +6,21 @@ var initialPlaces = [
   // {
   //   name: ,
   //   type: ,
-  //   latitude: ,
-  //   longitude: ,
+  //   position: {lat: 45.466342, lng: 9.188288},
   //   address:
   // }
 ];
 
 var Place = function(data){
-  this.latitude = data.latitude;
-  this.longitude = data.longitude;
+  this.position = data.position;
   this.name = data.name;
   this.address = data.address;
   this.type = data.type;
+  this.marker = new google.maps.Marker({
+    position: this.position,
+    title: this.name
+  });
+  this.marker.setMap(map);
 };
 
 // viewmodel
@@ -36,8 +29,23 @@ var ViewModel = function () {
 
   self.placeList = ko.observableArray([]);
 
+  self.currentFilter = ko.observable();
+  self.enableRestaurant = ko.observable();
+  self.enableMonuments = ko.observable();
+  self.enableNightlife = ko.observable();
+
   initialPlaces.forEach(function(placeItem) {
     self.placeList.push(new Place(placeItem));
+  });
+
+  self.filterPlaces = ko.computed(function() {
+    if(!self.currentFilter()) {
+      return self.placeList();
+    } else {
+      return ko.utils.arrayFilter(self.placeList(), function(place) {
+        return ~place.name.indexOf(self.currentFilter()) !== 0;
+      });
+    }
   });
 
   self.currentPlace = ko.observable();
